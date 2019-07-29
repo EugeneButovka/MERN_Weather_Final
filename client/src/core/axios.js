4a1219957b61dd85d1120b1dd00f344c82ac3951
EugeneButovka <eugene.butovka@gmail.com>
2019-07-29 14:19:19 +0300

    change axios connection

--- client/src/core/axios.js
+++ client/src/core/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // instance for local server - DB for users and queries
 export const instance = axios.create({
-    //baseURL: 'http://localhost:5000'
+    baseURL: (process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:5000')
 });
 
 // Alter defaults after instance has been created
